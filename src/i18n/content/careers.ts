c811c336450b6c9538a99c92b39255e6281ca327
emranaziziwebsite <emranazizi707@gmail.com>
2026-08-19 11:18:55 +0200

    Remove all files from repository

--- src/i18n/content/careers.ts
+++
@@ -1,9 +0,0 @@
-import { Locale } from "../locales";
-import { CareerTranslation } from "./types";
-import { careersDe } from "./careers.de";
-import { careersFa } from "./careers.fa";
-
-export const careerTranslations: Partial<Record<Locale, Record<string, CareerTranslation>>> = {
-  de: careersDe,
-  fa: careersFa,
-};
--- src/i18n/content/items.ts
+++
@@ -1,9 +0,0 @@
-import { Locale } from "../locales";
-import { ItemTranslation } from "./types";
-import { itemsDe } from "./items.de";
-import { itemsFa } from "./items.fa";
-
-export const itemTranslations: Partial<Record<Locale, Record<string, ItemTranslation>>> = {
-  de: itemsDe,
-  fa: itemsFa,
-};
